3e6fb1d59b80dd96e409703780cdfc688e595bb8
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-03-10 19:21:25 +0300

    feat: update blog

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -6,8 +6,11 @@ import Link from 'next/link';
 
 export default function Blog() {
   const path=usePathname();
+  const ids=path.split("/")
+  const id= ids[2];
   return (
     <div className={styles.body}>
+      <p>blog {id}</p>
       <p>So about this blog i don't really know what talk about it. It's just a test about route in next </p>
       <Link href={`${path}/image`}> Click here  image</Link>
     </div>
